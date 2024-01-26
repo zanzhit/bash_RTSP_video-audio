47321c02c3bfd4efb4f8287e8d1238e167e87d64
zanzhit <zangit@yandex.ru>
2024-01-26 14:38:19 +0300

    Проверить на rtsp

--- BASH_Vid+Aud.ps1
+++ BASH_Vid+Aud.ps1
@@ -1,9 +1,37 @@
-$rtsp_Vid = Read-Host "Enter rtsp for the video"
+# Функция для проверки rtsp URL
+function Test-RtspUrl {
+    param (
+        [string]$url
+    )
+    
+    # Регулярное выражение для проверки rtsp URL
+    $rtspRegex = '^rtsp:\/\/'
 
-$rtsp_Aud = Read-Host "Enter rtsp for the audio"
+    # Проверка
+    if ($url -match $rtspRegex) {
+        return $true
+    } else {
+        return $false
+    }
+}
 
-$command = gst-launch-1.0 rtspsrc location=$rtsp_Vid ! rtph264depay ! h264parse ! matroskamux name=mux ! filesink location=output.mkv rtspsrc location=$rtsp_Aud ! rtpmp4gdepay ! aacparse ! mux.
+# Запрос ввода rtsp URL для видео
+$rtsp_Vid = ""
+while (-not (Test-RtspUrl -url $rtsp_Vid)) {
+    $rtsp_Vid = Read-Host "Enter video rtsp"
+}
 
-Write-Output "Running: $command"
+# Запрос ввода rtsp URL для аудио
+$rtsp_Aud = ""
+while (-not (Test-RtspUrl -url $rtsp_Aud)) {
+    $rtsp_Aud = Read-Host "Enter audio rtsp"
+}
 
+# Формирование команды gstreamer
+$command = "gst-launch-1.0 rtspsrc location=$rtsp_Vid ! rtph264depay ! h264parse ! matroskamux name=mux ! filesink location=output.mkv rtspsrc location=$rtsp_Aud ! rtpmp4gdepay ! aacparse ! mux."
+
+# Вывод сформированной команды
+Write-Output "Running $command"
+
+# Запуск команды
 Invoke-Expression $command
